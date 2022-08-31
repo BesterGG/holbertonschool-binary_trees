fbad0b45dc868805ad44e9c3954a49b54f01ac9b
Bester <besster.gg@gmail.com>
2022-08-31 20:48:18 -0300

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,8 +8,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->left && tree->right)
+	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (1);
-	else
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	if (!tree->left && !tree->right)
+		return (1);	
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
