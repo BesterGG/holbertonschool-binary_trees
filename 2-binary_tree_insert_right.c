077c3d8cc8ebc0a0ebd254660eb6045194c5f634
Bester <besster.gg@gmail.com>
2022-08-31 14:08:16 -0300

    task2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - binary insert left side
+ * @parent: parent of node
+ * @value: value
+ * Return: node otherwise null
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = NULL;
+
+	if (!parent)
+		return (NULL);
+	node = malloc(sizeof(struct binary_tree_s));
+	if (!node)
+		return (NULL);
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+	node->parent = parent;
+	if (parent->right)
+	{
+		node->right = parent->right;
+		node->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
+}
