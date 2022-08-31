cbe7100693847b9da7d1c578fab73529b1184236
Jose Alvarez <5135@holbertonschool.com>
2022-08-31 09:13:55 -0700

    task0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = NULL;
+
+    node = malloc (sizeof(struct binary_tree_s));
+    if (!node)
+        return (NULL);
+    node->left = NULL;
+    node->right = NULL;
+    node->n = value;
+
+    if (!parent)
+        return(node);
+    else
+    {
+        if (value < node->n)
+        {
+            parent->left = node;
+            return (node);
+        }
+        else if (parent->n > node->n)
+        {
+        parent->right = node;
+        return (node);
+        }
+    }
+    return (NULL);
+}
