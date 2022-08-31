392a90d1f9c5ff40f0fe1b1fb9078e8ebadd1d14
Bester <besster.gg@gmail.com>
2022-08-31 15:11:32 -0300

    task8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - binary tree postorder print
+ * @tree: pointer to root
+ * @func: function print num
+ * Return: Void function
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
