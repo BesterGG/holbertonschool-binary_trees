35c0020b32ba17926145003997a611b9094ca34f
Bester <besster.gg@gmail.com>
2022-08-31 20:49:25 -0300

    betty fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,6 +11,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if ((!tree->left && tree->right) || (tree->left && !tree->right))
 		return (1);
 	if (!tree->left && !tree->right)
-		return (1);	
+		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
