f792a8d0a1a2d03e1af15247e61c014927ebb828
Bester <besster.gg@gmail.com>
2022-08-31 19:21:16 -0300

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,22 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+	return (maxdeep(tree));
+}
+int maxdeep(const binary_tree_t *tree)
+{
 	int lDepth = 0, rDepth = 0;
 
 	if (tree == NULL)
 	{
 		return (-1);
 	}
-		lDepth = binary_tree_height(tree->left);
-		rDepth = binary_tree_height(tree->right);
+		lDepth = maxdeep(tree->left);
+		rDepth = maxdeep(tree->right);
 		if (lDepth > rDepth)
-			return ((size_t)lDepth + 1);
+			return (lDepth + 1);
 		else
-			return ((size_t)rDepth + 1);
+			return (rDepth + 1);
 }
